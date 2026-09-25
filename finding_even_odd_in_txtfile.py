f3d4e0389892de139cbd430f5bfc41edf28357e9
Rohitdoctm <shawr7616@gmail.com>
2026-09-25 10:18:17 -0700

    adding comments to find odd digits

--- finding_even_odd_in_txtfile.py
+++ finding_even_odd_in_txtfile.py
@@ -13,3 +13,4 @@ def count_Even():
     return count
 ans=count_Even()
 print("The number of even digit present in the number.txt file is=",ans)
+# to find the no of odd digits in number.txt file just change the if condition %2!=0
